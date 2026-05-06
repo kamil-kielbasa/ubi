392faa6a6e16d4f4c710fc3f90b9bdd81df8580c
Kamil Kielbasa <kamkie1996@gmail.com>
2026-05-06 18:22:02 +0000

    chore: version mismatch

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ _buildoc_path = (_confdir / '..' / 'build' / 'doc' / 'doxygen').resolve()
 project = 'UBI on Zephyr'
 copyright = '2026, Kamil Kielbasa'
 author = 'Kamil Kielbasa'
-version = '0.59.0'
+version = '0.58.0'
 
 # -- General configuration ---------------------------------------------------
 
